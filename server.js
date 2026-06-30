d6c72870281f6bd20c07b58331fe6e7a31c94974
pegomatongo-rgb <pegomatongo@gmail.com>
2026-06-30 22:39:03 +0200

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Server is running");
 });
 const transporter = nodemailer.createTransport({
